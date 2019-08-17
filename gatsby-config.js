e1471f2a56f914696f9677aea1fdeb528f8b0f6e
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-08-17 13:47:07 +0200

    Install and config gatsby-source-wordpress plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: "localhost",
+        protocol: "http",
+        hostingWPCOM: false,
+        useACF: true,
+      },
+    },
+  ],
 }
